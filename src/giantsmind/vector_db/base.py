a1af7188845dd4c3a3826d714f88b163b3bd0b2c
Pierre Enel <pierre.enel@gmail.com>
2024-10-23 19:25:17 -0400

    WIP

--- src/giantsmind/vector_db/base.py
+++ src/giantsmind/vector_db/base.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from abc import ABC, abstractmethod
 from langchain_core.documents.base import Document
 
@@ -7,8 +7,4 @@ class VectorDBClient(ABC):
 
     @abstractmethod
     def check_ids_exist(self, IDs: List[str]) -> List[bool]:
-        pass
-
-    @abstractmethod
-    def add_documents(self, documents: List[Document]) -> List[str]:
         pass
